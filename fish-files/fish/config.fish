d2b7bd31345bd4fd200c7c3e1a02213f2610f707
Andrii Mulyk <mulikneo@gmail.com>
2025-04-21 12:47:01 +0300

    Update config.fish

--- fish-files/fish/config.fish
+++ fish-files/fish/config.fish
@@ -1,9 +1,1 @@
 oh-my-posh init fish --config $HOME/.config/poshthemes/montys.omp.json | source
-
-set fish_greeting
-
-function fish_prompt
-    set_color cyan
-    echo -n 'λ '
-    set_color normal
-end
