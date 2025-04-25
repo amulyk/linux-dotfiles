aa591a45be153b668956fb4d1c4a2a4775e7c35e
Andrii Mulyk <mulikneo@gmail.com>
2025-04-25 10:47:50 +0300

    Update config.fish

--- fish-files/fish/config.fish
+++ fish-files/fish/config.fish
@@ -2,8 +2,4 @@ oh-my-posh init fish --config $HOME/.config/poshthemes/montys.omp.json | source
 
 set fish_greeting
 
-function ai
-    source ~/.venvs/claude/bin/activate.fish
-    claude-code
-    deactivate
-end
+nvm use 20.15.0 > /dev/null
